b38a255653faec910d2f77e7eb2e773820536fe7
Blake <83431780+blakehoosline@users.noreply.github.com>
2023-02-14 00:09:49 -0600

    Update parseCmdLine.c

--- C/argParsing/parseCmdLine.c
+++ C/argParsing/parseCmdLine.c
@@ -13,8 +13,17 @@ int main(int argc, char *argv[])
     // Parse command line arguments
     char *team1 = argv[1];
     char *team2 = argv[2];
-    int score1 = atoi(argv[3]);
-    int score2 = atoi(argv[4]);
+    int score1, score2;
+    if (argc == 5)
+    {
+        score1 = atoi(argv[3]);
+        score2 = atoi(argv[4]);
+    }
+    else
+    {
+        score1 = atoi(argv[5]);
+        score2 = atoi(argv[6]);
+    }
 
     // Determine winner and score difference
     int diff = abs(score1 - score2);
@@ -22,7 +31,7 @@ int main(int argc, char *argv[])
     char *loser = (score1 > score2) ? team2 : team1;
 
     // Output result
-    printf("%s Won\n%d points Difference\n%c%c Team Lost\n", winner, diff, loser[0], loser[1]);
+    printf("%s Won %d points Difference %c%c Team Lost.\n", winner, diff, loser[0], loser[1]);
 
     return 0;
 }
